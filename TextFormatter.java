3949512c5031715d50015c84462293da5c0254a1
repl.it user <replituser@example.com>
2020-10-27 20:27:52 +0000

    Aufgabe fertig ohne Erweiterung

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,4 +1,9 @@
+import javax.xml.bind.MarshalException;
+
 class TextFormatter {
+
+  private int max;
+  private int counter;
 
   private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
           "eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et " +
@@ -15,12 +20,24 @@ class TextFormatter {
 
   // Konstruktor
   public TextFormatter(int maxLineLength) {
-    // ...
+    
+    max = maxLineLength;
+
   }
 
   // Ausgabe
   public void print(String aText) {
-    System.out.println("Hier sollte der Text mit passendem Umbruch erscheinen.");
+    for (int i=1; i<= aText.length(); i++){
+        if (i % max != 0){
+          System.out.print(aText.charAt(i-1));
+        } else{
+          if(aText.charAt(i-1) == ' '){
+            System.out.print("\n");
+          }else{
+            System.out.print(aText.charAt(i-1));
+            System.out.print("-\n");
+          }
+        }
+    }
   }
-
 }
